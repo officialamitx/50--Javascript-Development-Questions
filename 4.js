3c5099c5569e3786d42c0490b7fc191be47d0106
AMIT <156303740+officialamitx@users.noreply.github.com>
2026-04-29 13:13:23 +0530

    Create a function that takes an array containing both numbers and strings, and returns a new array containing only the string values.

--- 4.js
+++ 4.js
@@ -1,14 +1,14 @@
 // 4 Create a function that takes an array containing both numbers and strings, and returns a new array containing only the string values.
 
-// let arr = [22,33,44,"Amit","Sanjay","Rohit",11,55,77]
+let arr = [22,33,44,"Amit","Sanjay","Rohit",11,55,77]
 
 
-// new_arr = arr.filter(
-//     (Element)=>{
-//         if(typeof Element == "string"){
-//             return Element
-//         }
-//     }
-// )
+new_arr = arr.filter(
+    (Element)=>{
+        if(typeof Element == "string"){
+            return Element
+        }
+    }
+)
 
 // console.log(new_arr)
