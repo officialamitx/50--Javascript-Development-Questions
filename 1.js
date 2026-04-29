ceee2f0c212bfef8ea735cf71a83b0c39cc0fc64
AMIT <156303740+officialamitx@users.noreply.github.com>
2026-04-29 11:49:09 +0530

    Create a function that returns the last element of an array.

---
+++ 1.js
@@ -0,0 +1,10 @@
+// Create a function that returns the last element of an array.
+
+
+arr = [1,2,3,4,5,6,7,100]
+
+function getLastElement(){return arr[arr.length-1]}
+
+a = getLastElement()
+
+console.log(a)
