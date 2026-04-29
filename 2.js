49690fad3040725edf43c8906c268b813aaedbd2
AMIT <156303740+officialamitx@users.noreply.github.com>
2026-04-29 11:58:04 +0530

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,10 @@
+// Find the combination of two arrays.
+
+let arr_1 = [22,33,44]
+let arr_2 = [55,66,77]
+let arr_3 = [88,99,11]
+
+let combination_arr = arr_1.concat(arr_2,arr_3)
+
+console.log(combination_arr)
+ 
