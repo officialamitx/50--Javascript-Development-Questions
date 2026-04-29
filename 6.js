906c356f0c4670064d85052a9cf36949496c2122
AMIT <156303740+officialamitx@users.noreply.github.com>
2026-04-29 13:00:36 +0530

    Write a function that returns the length of a given object (number of keys).

---
+++ 6.js
@@ -0,0 +1,11 @@
+// 6 Write a function that returns the length of a given object (number of keys).
+
+// let arr ={
+//     name : "Amit",
+//     age : 19,
+//     city : "Delhi"
+// }
+
+// new_arr = Object.keys(arr).length
+
+// console.log(new_arr)
